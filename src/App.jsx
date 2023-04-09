5043b0d8c824bf167d1b6b4fe598ace794bd3495
Milan <milanpaulson8@gmail.com>
2023-04-09 22:29:23 +0530

    Revert "initial commit"
    This reverts commit f186f8cf20a733d42fa4a47cb1f6060be21620d5.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import { useState } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import {
-  OrbitControls,
-  PerspectiveCamera,
-  useTexture,
-} from "@react-three/drei";
-// import { GLTFLoader } from "three/src/loaders/GLTFLoader";
-import { BackSide, DoubleSide, TextureLoader } from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  const texture = useLoader(TextureLoader, "room.jpg");
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Canvas>
-        <mesh rotation-y={89.9}>
-          <sphereGeometry args={[500, 60, 40]} />
-          <meshBasicMaterial map={texture} side={BackSide} />
+        <mesh>
+          <boxGeometry />
+          <meshNormalMaterial />
         </mesh>
-
-        <OrbitControls dampingFactor={1} />
+        <OrbitControls />
       </Canvas>
     </>
   );
